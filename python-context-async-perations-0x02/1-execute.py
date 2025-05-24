0ed2281e1e5e96274ff42f0885adb72c8af32d71
Florence Ndabambi <flossiendabambi@gmail.com>
2025-05-24 10:27:50 +0200

    Create 1-execute.py

---
+++ python-context-async-perations-0x02/1-execute.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Custom context manager to execute a SQL query
+"""
+
+import sqlite3
+
+
+class ExecuteQuery:
+    def __init__(self, query, params=()):
+        self.query = query
+        self.params = params
+        self.conn = None
+        self.cursor = None
+
+    def __enter__(self):
+        # Establish the connection and cursor
+        self.conn = sqlite3.connect("my_database.db")
+        self.cursor = self.conn.cursor()
+
+        # Execute the query
+        self.cursor.execute(self.query, self.params)
+
+        # Fetch results
+        return self.cursor.fetchall()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # Close the cursor and connection
+        if self.cursor:
+            self.cursor.close()
+        if self.conn:
+            self.conn.close()
