14d292325f51193c6a85374a66a9f09cead5254a
Florence Ndabambi <flossiendabambi@gmail.com>
2025-05-28 15:33:28 +0200

    Update models.py

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,24 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+class User (models.Model):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=254)
+    last_name = models.CharField(max_length=254)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=128)
+    models.CharField(max_length=20)
+    
+    
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField("User")
+
+
+
+class Message(models.Model):
+    message_id = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name="messages")
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
