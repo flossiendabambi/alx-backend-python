430882fe28fc684dad494eef4eff7a74f514c47c
Florence Ndabambi <flossiendabambi@gmail.com>
2025-05-28 15:37:33 +0200

    Update models.py

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -8,7 +8,7 @@ class User (models.Model):
     last_name = models.CharField(max_length=254)
     email = models.EmailField(max_length=254)
     password = models.CharField(max_length=128)
-    models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20)
     
     
 class Conversation(models.Model):
