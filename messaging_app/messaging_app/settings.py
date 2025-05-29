980c178da30fbb48903c7ec148d982cdcdc6b20c
Florence Ndabambi <flossiendabambi@gmail.com>
2025-05-29 13:57:14 +0200

    Edit views.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -47,7 +47,10 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-    ]
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
 }
 
 MIDDLEWARE = [
