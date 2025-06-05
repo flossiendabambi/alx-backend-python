6b419af90d8784ab0e9e800febc4aafa07574837
Florence Ndabambi <flossiendabambi@gmail.com>
2025-06-05 19:27:07 +0200

    updates

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+        'chats.permissions.IsParticipantOfConversation',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
