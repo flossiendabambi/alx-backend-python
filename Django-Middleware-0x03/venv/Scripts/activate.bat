a93f72db5fe4c479c477b39dbcdb67ba958a5f42
Florence Ndabambi <flossiendabambi@gmail.com>
2025-06-07 19:12:08 +0200

    updates

--- Django-Middleware-0x03/venv/Scripts/activate.bat
+++ Django-Middleware-0x03/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\LENOVO\desktop\alx-backend-python\messaging_app\venv
+set VIRTUAL_ENV=C:\Users\LENOVO\desktop\alx-backend-python\Django-Middleware-0x03\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
