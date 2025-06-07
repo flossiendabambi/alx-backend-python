d93fbe76688c63e641b0ed9521899f943c6bdc0c
Florence Ndabambi <flossiendabambi@gmail.com>
2025-06-07 16:17:48 +0200

    restructure

---
+++ Django-Middleware-0x03/Django-Middleware-0x03/chats/views.py
@@ -0,0 +1,67 @@
+from rest_framework import viewsets, permissions
+from .models import User, Conversation, Message
+from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
+from .permissions import IsParticipantOfConversation
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import MessageFilter
+from rest_framework import status
+from rest_framework.response import Response
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    http_method_names = ['get', 'post', 'head']
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated, IsParticipantOfConversation]
+
+    def get_queryset(self):
+        return self.queryset.filter(participants=self.request.user)
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated, IsParticipantOfConversation]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = MessageFilter
+
+    def get_queryset(self):
+        return Message.objects.filter(conversation__participants=self.request.user)
+
+    
+    def create(self, request, *args, **kwargs):
+        conversation_id = request.data.get("conversation_id")
+        message_body = request.data.get("message_body")
+
+        if not conversation_id or not message_body:
+            return Response(
+                {"error": "Both conversation_id and message_body are required."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            conversation = Conversation.objects.get(conversation_id=conversation_id)
+        except Conversation.DoesNotExist:
+            return Response(
+                {"error": "Conversation not found."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        if request.user not in conversation.participants.all():
+            return Response(
+                {"error": "You are not a participant in this conversation."},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        message = Message.objects.create(
+            conversation=conversation,
+            sender=request.user,
+            message_body=message_body
+        )
+        serializer = self.get_serializer(message)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
