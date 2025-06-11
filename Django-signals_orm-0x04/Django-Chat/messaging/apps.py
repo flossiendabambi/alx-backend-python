f4b29f22254f2a09e2ab12b09f24649bcd5bd8af
Florence Ndabambi <flossiendabambi@gmail.com>
2025-06-11 10:14:23 +0200

    Delete Django-signals_orm-0x04/Django-Chat directory

--- Django-signals_orm-0x04/Django-Chat/messaging/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-class MessagingAppConfig(AppConfig):
-    name = 'messaging_app'
-
-    def ready(self):
-        import messaging.signals
--- Django-signals_orm-0x04/Django-Chat/messaging/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import Message, Notification
-
-@receiver(post_save, sender=Message)
-def create_notification_for_new_message(sender, instance, created, **kwargs):
-    if created:
-        Notification.objects.create(user=instance.receiver, message=instance)
-        print(f"🔔 Notification created for {instance.receiver.username}")
