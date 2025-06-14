77fd07a8add3cd0008f0930c0db657fa6afa615c
Florence Ndabambi <flossiendabambi@gmail.com>
2025-06-14 16:09:55 +0200

    Remove embedded Django-Chat repo from index

--- Django-signals_orm-0x04/djangochat/messaging/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('conversation/<int:conversation_id>/', views.conversation_detail),
-    path('inbox/', views.inbox, name='inbox'),
-]
